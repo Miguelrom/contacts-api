9b4e1014d644cd1779346c8aac62865a11470b2d
Miguel Romero <Miguelrom@users.noreply.github.com>
2024-04-11 12:19:52 -0700

    Add DB setup

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 import 'dotenv/config';
 import express from "express";
 import helmet from "helmet";
+import mongoose from 'mongoose'
+import { connectDB } from './config/dbConn.js';
 
 const app = express();
+connectDB();
 
 app.use(helmet());
 
 const PORT = process.env.PORT || 3001;
 
+mongoose.connection.on("open", () => {
+  console.log("MongoDB connected");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}...`);
-})
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB disconnected');
+});
+
+mongoose.connection.on('error', (error) => {
+  console.log(error);
+});
+
